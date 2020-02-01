84cdc9d6fbf572427004082c65354cbcd4018f98
s4rv4d <sarvadshetty@gmail.com>
2020-02-01 08:10:24 +0530

    finished basic understanding

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,44 +10,43 @@ import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
 
-struct ContentView: View {
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveError), nil)
+    }
+    
+    @objc func saveError(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save Finished")
+    }
+}
 
+struct ContentView: View {
+    
     @State private var image: Image?
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
     
     var body: some View {
         VStack {
             image?
-            .resizable()
+                .resizable()
             .scaledToFit()
+            
+            Button("Select image") {
+                self.showingImagePicker = true
+            }
+        }.sheet(isPresented: $showingImagePicker, onDismiss: loadImage) {
+            ImagePicker(image: self.$inputImage)
         }
-    .onAppear(perform: loadExample)
     }
     
-    func loadExample() {
-        guard let inputImage = UIImage(named: "Example") else { return }
-        let beginImage = CIImage(image: inputImage)
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
         
-        let context = CIContext()
-//        let currentFilter = CIFilter.crystallize()
-//
-//        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-//        currentFilter.radius = 200
-        
-        guard let currentFilter = CIFilter(name: "CITwirlDistortion") else { return }
-        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
-        currentFilter.setValue(200, forKey: kCIInputRadiusKey)
-        currentFilter.setValue(CIVector(x: inputImage.size.width / 2, y: inputImage.size.height / 2), forKey: kCIInputCenterKey)
-        
-        //steps to get Image
-        //get CIImage from our filter output
-        guard let outputImage = currentFilter.outputImage else { return }
-        //attempt to get CGImage from CIImage
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            //convert cgimage into uiimage
-            let uiimage = UIImage(cgImage: cgimg)
-            //convert uiimage into image
-            image = Image(uiImage: uiimage)
-        }
+        //saving image
+        let imageSaver = ImageSaver()
+        imageSaver.writeToPhotoAlbum(image: inputImage)
     }
 }
 
---
+++ Instafilter/ImagePicker.swift
@@ -0,0 +1,48 @@
+//
+//  ImagePicker.swift
+//  Instafilter
+//
+//  Created by Sarvad shetty on 1/31/20.
+//  Copyright © 2020 Sarvad shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ImagePicker: UIViewControllerRepresentable {
+//    typealias UIViewControllerType = UIImagePickerController
+    
+    //coordinator class
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        var parent: ImagePicker
+        
+        init(_ parent:ImagePicker) {
+            self.parent = parent
+        }
+        
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let uiimage = info[.originalImage] as? UIImage {
+                parent.image = uiimage
+            }
+            //then dismiss after selecting
+            parent.presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    //lets us detect if image was selected in contentView.swift
+    @Binding var image: UIImage?
+    @Environment(\.presentationMode) var presentationMode
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.delegate = context.coordinator
+        return picker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+        
+    }
+}
